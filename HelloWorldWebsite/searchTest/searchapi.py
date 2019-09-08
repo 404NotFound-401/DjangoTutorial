0c184f6d291959140250af528521747bcb40f0c4
Vincentlcy <liuchuyang1997@outlook.com>
2019-09-08 10:42:19 -0600

    backend down, need to merge

--- HelloWorldWebsite/searchTest/searchapi.py
+++ HelloWorldWebsite/searchTest/searchapi.py
@@ -8,4 +8,20 @@ def getName(name):
     api_key = '&apikey=d6f684e4'
     search = path + processName + api_key
     r = requests.get(search)
-    return r.text
+    moive_info = string_process(r.text)
+    return moive_info
+
+
+def string_process(text_origin):
+    moive_info = {}
+    text_origin = text_origin[2:len(text_origin)-2]
+    elements_list = text_origin.split('","')
+
+    for element in elements_list:
+        keywords = element.split('":"')
+        moive_info[keywords[0]] = keywords[1]
+    
+    ratings = moive_info["Value"]
+    moive_info["Value"] = ratings[:6]
+    return moive_info
+
